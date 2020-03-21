c4fd7f59ae4f2fea94d80bdb826cdc8dece0f425
leangaurav <trainer.cpp@gmail.com>
2020-03-21 12:16:15 +0530

    Session 6 notebooks

---
+++ Scripts/f3_module/f1.py
@@ -0,0 +1,7 @@
+def square(x):
+    return x**2
+
+if __name__ == '__main__':
+    print("Hello from f1:",  __name__)
+    print(square(100))
+    print(square(-100))
---
+++ Scripts/f3_module/f2.py
@@ -0,0 +1,4 @@
+import f1
+
+print("Hello from f2:",  __name__)
+print(f1.square(10))
